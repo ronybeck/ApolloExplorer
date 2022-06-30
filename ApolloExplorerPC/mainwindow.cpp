2626fc4193de2b9e70ea6796e96ed14345ba2e61
ronybeck <ronybeck@themenz.biz>
2022-06-30 12:58:14 +0200

    - Improving the timeout mechanism.  Now, traffic on the existing connection are counted in addition to the "Hello" sent over the broadcast.

--- ApolloExplorerPC/mainwindow.cpp
+++ ApolloExplorerPC/mainwindow.cpp
@@ -100,7 +100,7 @@ MainWindow::MainWindow( QSharedPointer<QSettings> settings, QSharedPointer<Amiga
     m_DialogUploadFile = QSharedPointer<DialogUploadFile>( new DialogUploadFile() );
 
     //Setup the custom views
-    m_FileTableView = new RemoteFileTableView();
+    m_FileTableView = new RemoteFileTableView( this );
     m_FileTableView->setDownloadDialog( m_DialogDownloadFile );
     m_FileTableView->setUploadDialog( m_DialogUploadFile );
     ui->verticalLayoutFileBrowser->addWidget( m_FileTableView );
@@ -300,11 +300,13 @@ void MainWindow::dropEvent(QDropEvent *e)
 
 void MainWindow::dragLeaveEvent(QDragLeaveEvent *e)
 {
+    Q_UNUSED( e )
     //qDebug() << "DragLeaveEvent";
 }
 
 void MainWindow::dragMoveEvent(QDragMoveEvent *e)
 {
+    Q_UNUSED( e )
     //qDebug() << "Drag move event";
 }
 
@@ -335,6 +337,7 @@ void MainWindow::onShowDrivesToggledSlot(bool enabled)
 
 void MainWindow::onListModeToggledSlot(bool enabled)
 {
+    Q_UNUSED( enabled )
     m_ViewType = VIEW_LIST;
     if( m_ViewType == VIEW_LIST )
     {
@@ -1016,6 +1019,7 @@ void MainWindow::onServerClosedConnectionSlot(QString message)
 void MainWindow::onIncomingByteCountUpdateSlot( quint32 bytes )
 {
     m_IncomingByteCount += bytes;
+    m_AmigaHost->setHostRespondedNow();
 }
 
 void MainWindow::onOutgoingByteCountUpdateSlot( quint32 bytes )
