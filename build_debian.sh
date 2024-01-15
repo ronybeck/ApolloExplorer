4fe5cabb1277d762c5f7ba4edbe0caa8c5972fd3
Ronnie Beck <ronybeck@themenz.biz>
2024-01-15 18:13:52 +0100

    Updates to Build and WhatsNew information

--- build_debian.sh
+++ build_debian.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 VERSION=$1
-VERSION=${VERSION:-'1.1.1'}
+VERSION=${VERSION:-'1.1.2'}
 BUILDDIR="apolloexplorer-${VERSION}"
 TARFILE="apolloexplorer_${VERSION}.orig.tar.gz"
 
 #cleanup any leftovers from last time
 echo "Cleaning the slate"
 rm -rf ${BUILDDIR} ${TARFILE}
+rm -f apolloexplorer_*amd64.build*
+rm -f apolloexplorer_*amd64.changes
+rm -f apolloexplorer_*.debian.tar.xz
+rm -f apolloexplorer_*.dsc
+rm -f apolloexplorer-dbgsym_*amd64.ddeb
 
 #Preparation
 echo "Preparing ${BUILDDIR}"
